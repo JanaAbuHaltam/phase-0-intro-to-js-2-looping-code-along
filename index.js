86919ca4b58fb657608e8e8498d452a066fac016
Jana Salah <janaabuhaltam@gmail.com>
2023-09-26 20:19:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const array = ["Charlie", "Samip", "Ali"];
+//const birthday = "birthday";
+function writeCards(array){
+    let messages =[];
+
+for (let i = 0; i < array.length ; i++) 
+   {
+    const name = array[i];
+    messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+
+   }
+   return messages;
+
+}
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
